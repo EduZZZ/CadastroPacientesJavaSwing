fee31125c63f7bf04d7d77eaf32f96d7cc6b5227
eduFeitosa <eduardofeitosa19@icloud.com>
2022-09-27 13:32:17 -0300

    add barra de menu

--- src/br/com/cadastroPacientes/model/Paciente.java
+++ src/br/com/cadastroPacientes/model/Paciente.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package br.com.cadastroPacientes.model;
 
-/**
- *
- * @author Eduardo
- */
 public class Paciente {
+    private String  nome;
+    private String dataNascimento;
+    private String sexo;
+    private String cardNum;
+
+    public Paciente() {
+        this.nome = "";
+        this.dataNascimento =  "";
+        this.sexo = "";
+        this.cardNum =  "";
+        
+    }
+
+    public Paciente(String nome, String dataNascimento, String sexo, String cardNum) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.cardNum = cardNum;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(String cardNum) {
+        this.cardNum = cardNum;
+    }
+    
+    
+    
+    
+    
+    
+            
+    
     
 }
